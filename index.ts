836d3e40d53d71a273583cb5bcb4765c4126215e
Martin Stoev <martin.y.stoev@gmail.com>
2023-05-19 11:25:56 +0300

    mongodb poc

--- index.ts
+++ index.ts
@@ -1,4 +1,20 @@
+import "dotenv/config";
 import fastify from "fastify";
+import { Schema, connect, model } from "mongoose";
+
+const { MONGODB_NAME, MONGODB_USER, MONGODB_PASS } = process.env;
+
+interface ITestDb {
+  name: string;
+  age: number;
+}
+
+const testDbSchema = new Schema<ITestDb>({
+  name: { type: String },
+  age: { type: Number },
+});
+
+const TestDb = model<ITestDb>("TestDb", testDbSchema, "logs");
 
 interface IQuerystring {
   username: string;
@@ -9,9 +25,21 @@ interface IHeaders {
   "h-Custom": string;
 }
 
-const server = fastify();
+const server = fastify({ logger: true });
+
+connect(
+  `mongodb+srv://${MONGODB_USER}:${MONGODB_PASS}@cluster0.tl0wald.mongodb.net/${MONGODB_NAME}?retryWrites=true&w=majority`
+)
+  .then(() => server.log.info("MongoDB connected..."))
+  .catch((err) => server.log.error(err));
 
 server.get("/ping", async () => "pong\n");
+
+server.get("/test-db", async (req, reply) => {
+  const result = await TestDb.create({ name: "test-create", age: 15 });
+  console.log(result);
+  return result;
+});
 
 server.get<{ Querystring: IQuerystring; Headers: IHeaders }>(
   "/auth",
