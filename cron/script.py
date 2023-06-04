00160dda47d8f70b4f952882e75f2edaa090bd05
Martin Stoev <martin.y.stoev@gmail.com>
2023-06-04 18:49:04 +0300

    format python

--- cron/script.py
+++ cron/script.py
@@ -6,43 +6,63 @@ from dotenv import load_dotenv
 
 
 def get_db_collection(connection_string, db_name, collection_name):
-  client = MongoClient(connection_string)
+    client = MongoClient(connection_string)
 
-  database = client[db_name]
-  collection = database[collection_name]
+    database = client[db_name]
+    collection = database[collection_name]
 
-  return collection
+    return collection
+
 
 def parseResponse(response):
-  [result, *rest] = response.json()
+    [result, *rest] = response.json()
 
-  temperature = next((x['value'] for x in result['sensordatavalues'] if x['value_type'] == "temperature"), None)
-  humidity = next((x['value'] for x in result['sensordatavalues'] if x['value_type'] == 'humidity'), None)
+    temperature = next(
+        (
+            x["value"]
+            for x in result["sensordatavalues"]
+            if x["value_type"] == "temperature"
+        ),
+        None,
+    )
+    humidity = next(
+        (
+            x["value"]
+            for x in result["sensordatavalues"]
+            if x["value_type"] == "humidity"
+        ),
+        None,
+    )
 
-  dt = datetime.strptime(result['timestamp'], '%Y-%m-%d %H:%M:%S')
-  timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
+    dt = datetime.strptime(result["timestamp"], "%Y-%m-%d %H:%M:%S")
+    timestamp = dt.replace(tzinfo=timezone.utc).timestamp()
 
-  return [timestamp, temperature, humidity]
+    return [timestamp, temperature, humidity]
 
 
 def main():
-  load_dotenv()
-  URL = os.getenv('SENSOR_URL')
-  CONNECTION_STRING = os.getenv('CONNECTION_STRING')
-  DATABASE_NAME = os.getenv('DATABASE_NAME')
-  DATABASE_COLLECTION = os.getenv('DATABASE_COLLECTION')
+    load_dotenv()
+    URL = os.getenv("SENSOR_URL")
+    CONNECTION_STRING = os.getenv("CONNECTION_STRING")
+    DATABASE_NAME = os.getenv("DATABASE_NAME")
+    DATABASE_COLLECTION = os.getenv("DATABASE_COLLECTION")
 
-  response = requests.get(url = URL)
-  [timestamp, temperature, humidity] = parseResponse(response)
+    response = requests.get(url=URL)
+    [timestamp, temperature, humidity] = parseResponse(response)
 
-  collection = get_db_collection(CONNECTION_STRING, DATABASE_NAME, DATABASE_COLLECTION)
+    collection = get_db_collection(
+        CONNECTION_STRING, DATABASE_NAME, DATABASE_COLLECTION
+    )
 
-  collection.insert_one({
-    'timestamp': int(timestamp),
-    'temperature': float(temperature),
-    'humidity': float(humidity)
-  })
+    collection.insert_one(
+        {
+            "timestamp": int(timestamp),
+            "temperature": float(temperature),
+            "humidity": float(humidity),
+        }
+    )
 
-  print("Successfuly added entry!")
+    print("Successfully added entry!")
+
 
 main()
