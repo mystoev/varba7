81e206fc92ea169a6162446dce730d7d4a84e7fd
Martin Stoev <martin.y.stoev@gmail.com>
2023-06-13 18:18:39 +0300

    add prop-types. Line chart to Bar

--- web/src/stats.jsx
+++ web/src/stats.jsx
@@ -1,38 +1,36 @@
 import dayjs from "dayjs";
-import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
+import PropTypes from "prop-types";
+import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
 import { useStats } from "./use-stats";
+
+const Chart = ({ data, field }) => (
+  <BarChart width={1680} height={400} data={data}>
+    <Bar type="monotone" dataKey={field} stroke="#8884d8" />
+    <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+    <XAxis
+      dataKey="timestamp"
+      tickFormatter={(timestamp) => {
+        const date = dayjs.unix(timestamp);
+        return `${date.date()} ${date.format("MMM")}`;
+      }}
+    />
+    <YAxis dataKey={field} />
+    <Tooltip labelFormatter={() => ""} />
+  </BarChart>
+);
+
+Chart.propTypes = {
+  data: PropTypes.array.isRequired,
+  field: PropTypes.string.isRequired,
+};
 
 const Stats = () => {
   const stats = useStats();
 
   return (
     <div>
-      <div>
-        <LineChart width={1680} height={400} data={stats}>
-          <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" />
-          <XAxis
-            dataKey="timestamp"
-            tickFormatter={(timestamp) => {
-              const date = dayjs.unix(timestamp);
-              return `${date.date()} ${date.format("MMM")}`;
-            }}
-          />
-          <YAxis dataKey="temperature" />
-        </LineChart>
-        <LineChart width={1680} height={400} data={stats}>
-          <Line type="monotone" dataKey="humidity" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" />
-          <XAxis
-            dataKey="timestamp"
-            tickFormatter={(timestamp) => {
-              const date = dayjs.unix(timestamp);
-              return `${date.date()} ${date.format("MMM")}`;
-            }}
-          />
-          <YAxis dataKey="humidity" />
-        </LineChart>
-      </div>
+      <Chart data={stats} field={"temperature"} />
+      <Chart data={stats} field={"humidity"} />
     </div>
   );
 };
