26d3f80ec2489b76faa935182592a3e979b26d54
Martin Stoev <martin.y.stoev@gmail.com>
2023-09-18 10:38:17 +0300

    refactor mongo model

--- packages/gateway/src/models/mongo.ts
+++ packages/gateway/src/models/mongo.ts
@@ -1,15 +1,17 @@
 import { Schema, model } from "mongoose";
 
-export interface IBME280 {
+interface IBME280 {
   timestamp: number;
   temperature: number;
   humidity: number;
 }
 
-export const BME280Schema = new Schema<IBME280>({
-  timestamp: { type: Number },
-  temperature: { type: Number },
-  humidity: { type: Number },
-});
-
-export const BME280 = model<IBME280>("Stats", BME280Schema, "stats");
+export const BME280 = model<IBME280>(
+  "Stats",
+  new Schema<IBME280>({
+    timestamp: { type: Number },
+    temperature: { type: Number },
+    humidity: { type: Number },
+  }),
+  "stats"
+);
