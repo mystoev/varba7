51368583185baf69fecd67d5adf8fff34196d152
Martin Stoev <martin.y.stoev@gmail.com>
2023-09-20 17:49:55 +0300

    add mobile navigation

--- packages/ui/mobile/App.tsx
+++ packages/ui/mobile/App.tsx
@@ -1,7 +1,16 @@
 import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
 import React from 'react';
-import {SafeAreaView, ScrollView, useColorScheme} from 'react-native';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  useColorScheme,
+} from 'react-native';
 import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 import Home from './app/shared/components/home';
 
@@ -10,7 +19,9 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
-function App(): JSX.Element {
+const Stack = createNativeStackNavigator();
+
+const HomePage = ({navigation}) => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -24,9 +35,33 @@ function App(): JSX.Element {
         style={backgroundStyle}>
         <ApolloProvider client={client}>
           <Home />
+          <Button
+            title="Go to Details"
+            onPress={() => navigation.navigate('Test')}
+          />
         </ApolloProvider>
       </ScrollView>
     </SafeAreaView>
+  );
+};
+
+const TestPage = ({navigation}) => {
+  return (
+    <ScrollView>
+      <Text>Next page</Text>
+      <Button title="Go Back" onPress={() => navigation.goBack()} />
+    </ScrollView>
+  );
+};
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomePage} />
+        <Stack.Screen name="Test" component={TestPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
