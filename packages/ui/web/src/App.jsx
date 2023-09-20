40090d1bc61ff6745083bd1d355915501036aa1f
Martin Stoev <martin.y.stoev@gmail.com>
2023-09-20 18:06:42 +0300

    add react router

--- packages/ui/web/src/App.jsx
+++ packages/ui/web/src/App.jsx
@@ -1,4 +1,5 @@
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { Link, RouterProvider, createMemoryRouter } from "react-router-dom";
 
 import Home from "./shared/components/home";
 
@@ -7,9 +8,38 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const HomePage = () => {
+  return (
+    <>
+      <Home />
+      <Link to="/test">Test Page</Link>
+    </>
+  );
+};
+
+const TestPage = () => {
+  return (
+    <>
+      <p>Test Page</p>
+      <Link to="/">Back Home</Link>
+    </>
+  );
+};
+
+const router = createMemoryRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/test",
+    element: <TestPage />,
+  },
+]);
+
 const App = () => (
   <ApolloProvider client={client}>
-    <Home />
+    <RouterProvider router={router} />
   </ApolloProvider>
 );
 
