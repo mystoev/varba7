e977a26a35654fc7a9a21938d58948fc86353c70
Martin Stoev <martin.y.stoev@gmail.com>
2023-09-21 16:26:38 +0300

    refactor

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 # TODO
 
-- Use TS for server; JavaScript for UI
 - Server should be Apollo GraphQL with Fastify integration. Set everything up without Fastify and then switch
 - Setup infra using Ansible
 - Move schema to gql/graphql file
--- packages/ui/mobile/app/shared/components/home.jsx
+++ packages/ui/mobile/app/shared/components/home.jsx
@@ -1,11 +1,9 @@
 import {View} from 'react-native';
 
-import {useSharedNavigation} from '../../navigation';
 import Badge_BME280 from './badge-bme280';
 import Badge_SDS011 from './badge-sds011';
 
 const Home = () => {
-  const {navigate} = useSharedNavigation();
   return (
     <View>
       <Badge_BME280 />
