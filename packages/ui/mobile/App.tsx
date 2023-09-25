c3ea61b2e183a0ea813330d73bd63bf2d3411944
Martin Stoev <martin.y.stoev@gmail.com>
2023-09-25 17:14:14 +0300

    .

--- packages/ui/mobile/App.tsx
+++ packages/ui/mobile/App.tsx
@@ -16,7 +16,7 @@ import {BME280Page, HomePage} from './app/shared/pages';
 const httpLink = createHttpLink({
   uri:
     process.env.NODE_ENV !== 'production'
-      ? 'http://176.12.23.79:6161'
+      ? 'http://10.0.2.2:4000' //'http://176.12.23.79:6161'
       : process.env.REACT_APP_GQL_SERVER,
 });
 
