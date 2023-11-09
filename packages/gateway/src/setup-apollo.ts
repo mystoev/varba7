938f91850a259a62b14d7a858e8adc1a8decba07
Martin Stoev <martin.y.stoev@gmail.com>
2023-11-09 13:58:51 +0200

    fix introspection

--- packages/gateway/src/setup-apollo.ts
+++ packages/gateway/src/setup-apollo.ts
@@ -1,8 +1,8 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import * as dotenv from "dotenv";
-import { GraphQLError } from "graphql";
 
+import { GraphQLError } from "graphql";
 import { PeriodicBME280Data } from "./datasources/bme280-stats";
 import { WeatherSensorsAPI } from "./datasources/weather-sensors-api";
 import resolvers from "./resolvers";
@@ -11,10 +11,12 @@ import typeDefs from "./schema";
 dotenv.config({ path: __dirname + "/../.env.local" });
 const { MOBILE_TOKEN, TOKEN, APP_PORT } = process.env;
 
+const isProd = process.env.NODE_ENV === "production";
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  introspection: process.env.NODE_ENV !== "production",
+  introspection: !isProd,
 });
 
 export const start = async () => {
@@ -23,12 +25,14 @@ export const start = async () => {
       const token = req.headers.authorization || "";
       const userToken = token.split(" ")[1];
 
-      if ([TOKEN, MOBILE_TOKEN].indexOf(userToken) === -1) {
-        throw new GraphQLError("You must be logged in!", {
-          extensions: {
-            code: "UNAUTHENTICATED",
-          },
-        });
+      if (isProd) {
+        if ([TOKEN, MOBILE_TOKEN].indexOf(userToken) === -1) {
+          throw new GraphQLError("You must be logged in!", {
+            extensions: {
+              code: "UNAUTHENTICATED",
+            },
+          });
+        }
       }
 
       const { cache } = server;
