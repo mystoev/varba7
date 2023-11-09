0f84237334738906293c647594f60a31ed9edf92
Martin Stoev <martin.y.stoev@gmail.com>
2023-11-09 13:33:32 +0200

    remove periodicBME280 cache

--- packages/gateway/src/datasources/bme280-stats.ts
+++ packages/gateway/src/datasources/bme280-stats.ts
@@ -1,17 +1,17 @@
 import { KeyValueCache } from "apollo-server-caching";
 import { BME280 } from "../models/mongo";
 
-const msTillNextHour = (): number => {
-  const now = new Date();
-  const year = now.getFullYear();
-  const month = now.getMonth() + 1;
-  const date = now.getDate();
-  const hours = now.getHours() + 1;
+// const msTillNextHour = (): number => {
+//   const now = new Date();
+//   const year = now.getFullYear();
+//   const month = now.getMonth();
+//   const date = now.getDate();
+//   const hours = now.getHours() + 1;
 
-  const nextHour = new Date(`${year}-${month}-${date} ${hours}:`);
+//   const nextHour = new Date(`${year}-${month}-${date} ${hours}:`);
 
-  return +nextHour - +now;
-};
+//   return +nextHour - +now;
+// };
 
 export class PeriodicBME280Data {
   private cache: KeyValueCache<string>;
@@ -21,17 +21,17 @@ export class PeriodicBME280Data {
   }
 
   async periodicBME280(startDate: number, endDate: number) {
-    const cacheKey = `BME280-${startDate}-${endDate}`;
-    const cacheObject = await this.cache.get(cacheKey);
-    if (cacheObject) {
-      return JSON.parse(cacheObject);
-    }
+    // const cacheKey = `BME280-${startDate}-${endDate}`;
+    // const cacheObject = await this.cache.get(cacheKey);
+    // if (cacheObject) {
+    //   return JSON.parse(cacheObject);
+    // }
 
     const result = await BME280.find({
       timestamp: { $gt: startDate, $lt: endDate },
     });
 
-    this.cache.set(cacheKey, JSON.stringify(result), { ttl: msTillNextHour() });
+    // this.cache.set(cacheKey, JSON.stringify(result), { ttl: msTillNextHour() });
 
     return result;
   }
