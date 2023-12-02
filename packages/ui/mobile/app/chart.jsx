b51412e7679fbf8663c9cbff096b4cb483f53c6d
Martin Stoev <martin.y.stoev@gmail.com>
2023-12-02 11:24:30 +0200

    add average temps chart

--- packages/ui/mobile/app/chart.jsx
+++ packages/ui/mobile/app/chart.jsx
@@ -4,16 +4,19 @@ import React from 'react';
 import {ScrollView, StyleSheet, View, processColor} from 'react-native';
 import {BarChart} from 'react-native-charts-wrapper';
 import {GET_PERIODIC_BME280} from './shared/queries/periodic-bme280';
-import {maxTemperatures} from './shared/selectors/temperature';
+import {
+  averageTemperatures,
+  maxTemperatures,
+} from './shared/selectors/temperature';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 400,
     margin: 10,
   },
   chart: {
     flex: 1,
+    height: 250,
   },
 });
 
@@ -27,6 +30,7 @@ const Chart = ({startDate, endDate}) => {
   }
 
   const maxTemps = maxTemperatures(data.periodicBME280, startDate, endDate);
+  const avgTemps = averageTemperatures(data.periodicBME280, startDate, endDate);
 
   return (
     <ScrollView>
@@ -40,10 +44,28 @@ const Chart = ({startDate, endDate}) => {
           data={{
             dataSets: [
               {
-                label: 'Temperature',
+                label: 'Max Temperatures',
                 values: maxTemps.map(d => d.temperature),
                 config: {
                   colors: [processColor('orangered')],
+                },
+              },
+            ],
+          }}
+        />
+        <BarChart
+          style={styles.chart}
+          xAxis={{
+            valueFormatter: avgTemps.map(d => format(d.timestamp, 'd')),
+            granularity: 1,
+          }}
+          data={{
+            dataSets: [
+              {
+                label: 'Average Temperatures',
+                values: avgTemps.map(d => d.temperature),
+                config: {
+                  colors: [processColor('green')],
                 },
               },
             ],
--- packages/ui/mobile/app/shared/selectors/temperature.js
+++ packages/ui/mobile/app/shared/selectors/temperature.js
@@ -1,5 +1,8 @@
 import {format} from 'date-fns';
 
+const byTimestamp = (a, b) =>
+  a.timestamp < b.timestamp ? -1 : a.timestamp > b.timestamp ? 1 : 0;
+
 export const maxTemperatures = data => {
   if (!data) {
     return [];
@@ -20,15 +23,41 @@ export const maxTemperatures = data => {
 
   const result = Object.values(temperaturesByDay);
 
-  result.sort((a, b) => {
-    if (a.timestamp < b.timestamp) {
-      return -1;
-    } else if (a.timestamp > b.timestamp) {
-      return 1;
-    }
-
-    return 0;
-  });
+  result.sort(byTimestamp);
 
   return result;
+};
+
+export const averageTemperatures = data => {
+  if (!data) {
+    return [];
+  }
+
+  const temperaturesByDay = {};
+  data.map(({timestamp, temperature}) => {
+    const key = format(timestamp * 1000, 'MMM-dd');
+    if (!temperaturesByDay[key]) {
+      temperaturesByDay[key] = {
+        timestamp: timestamp * 1000,
+        temperature,
+        count: 0,
+      };
+      return;
+    }
+
+    temperaturesByDay[key].temperature += temperature;
+    temperaturesByDay[key].count += 1;
+  });
+
+  const result = Object.values(temperaturesByDay);
+
+  result.sort(byTimestamp);
+
+  console.log(result);
+  return result.map(r => {
+    return {
+      timestamp: r.timestamp,
+      temperature: r.temperature / r.count,
+    };
+  });
 };
