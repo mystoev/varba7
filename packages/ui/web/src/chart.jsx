d4fa61ff94e28e6102dc8ae734ef87e6b72f9ae1
Martin Stoev <martin.y.stoev@gmail.com>
2023-12-19 21:22:42 +0200

    fix chart width

--- packages/ui/web/src/chart.jsx
+++ packages/ui/web/src/chart.jsx
@@ -2,7 +2,15 @@ import { useQuery } from "@apollo/client";
 import { format } from "date-fns";
 import React from "react";
 import { View } from "react-native";
-import { Bar, BarChart, CartesianGrid, Tooltip, XAxis, YAxis } from "recharts";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { GET_PERIODIC_BME280 } from "./shared/queries/periodic-bme280";
 import {
   averageTemperatures,
@@ -23,36 +31,40 @@ const Chart = ({ startDate, endDate }) => {
 
   return (
     <View>
-      <BarChart
-        data={maxTemps}
-        width={800}
-        height={400}
-        style={{ margin: "auto", marginTop: 10 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="timestamp"
-          tickFormatter={(item) => format(item, "dd-MMM")}
-        />
-        <YAxis />
-        <Tooltip labelFormatter={(item) => format(item, "dd MMM, HH:mm")} />
-        <Bar dataKey={"temperature"} fill={"orangered"} />
-      </BarChart>
-      <BarChart
-        data={avgTemps}
-        width={800}
-        height={400}
-        style={{ margin: "auto", marginTop: 10 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis
-          dataKey="timestamp"
-          tickFormatter={(item) => format(item, "dd-MMM")}
-        />
-        <YAxis />
-        <Tooltip labelFormatter={(item) => format(item, "dd MMM")} />
-        <Bar dataKey={"temperature"} fill={"green"} />
-      </BarChart>
+      <ResponsiveContainer width="100%" aspect={1.0 / 0.4}>
+        <BarChart
+          data={maxTemps}
+          width={800}
+          height={400}
+          style={{ margin: "auto", marginTop: 10 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="timestamp"
+            tickFormatter={(item) => format(item, "dd-MMM")}
+          />
+          <YAxis />
+          <Tooltip labelFormatter={(item) => format(item, "dd MMM, HH:mm")} />
+          <Bar dataKey={"temperature"} fill={"orangered"} />
+        </BarChart>
+      </ResponsiveContainer>
+      <ResponsiveContainer width="100%" aspect={1.0 / 0.4}>
+        <BarChart
+          data={avgTemps}
+          width={800}
+          height={400}
+          style={{ margin: "auto", marginTop: 10 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis
+            dataKey="timestamp"
+            tickFormatter={(item) => format(item, "dd-MMM")}
+          />
+          <YAxis />
+          <Tooltip labelFormatter={(item) => format(item, "dd MMM")} />
+          <Bar dataKey={"temperature"} fill={"green"} />
+        </BarChart>
+      </ResponsiveContainer>
     </View>
   );
 };
