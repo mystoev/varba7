749b53d4c37a8eaec2d330eeee6ff4c92df869bb
Martin Stoev <martin.y.stoev@gmail.com>
2024-02-19 16:04:16 +0200

    remove mobile

--- packages/ui/mobile/app/navigation.jsx
+++
@@ -1,9 +0,0 @@
-import {useNavigation} from '@react-navigation/native';
-
-export const useSharedNavigation = () => {
-  const {navigate} = useNavigation();
-
-  return {
-    navigate: pageName => navigate(pageName),
-  };
-};
